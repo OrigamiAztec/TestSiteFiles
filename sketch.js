74f0f8a08925dbcec8e863c76ae148a30cb2675f
Antonio Diaz <origamiaztec@gmail.com>
2019-06-14 12:01:50 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,5 +3,26 @@ function setup() {
 }
 
 function draw() {
-
 }
+
+async function getData(){
+			const response = await fetch('testing.csv');
+			const data = await response.text();
+			console.log(data);
+	
+			const rows = data.split('\n').slice(1, 140);
+	
+			rows.forEach(element => {
+				const row = element.split(',');
+				const year = row[0];
+				xlabels.push(year);
+				const meanTemp = row[1];
+				meantemparray.push(parseFloat(meanTemp)*9/5 + 32);
+				console.log(year, meanTemp);
+			});
+
+		}
+		
+getData();
+
+
