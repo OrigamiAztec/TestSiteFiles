a495b790e566314e9fbd4614677b9e0336db6efb
Antonio Diaz <origamiaztec@gmail.com>
2019-06-14 12:33:36 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -23,6 +23,35 @@ async function getData(){
 
 		}
 		
-getData();
+
+async function graphData(){
+	await getData();
+	const ctx = document.getElementById('myChart').getContext('2d');
+		const myChart = new Chart(ctx, {
+			type: 'line',
+			data: {
+				labels: xlabels,
+				datasets: [{
+					label: 'Combined Land-Surface Air and Sea-Surface Water Temperature (F)',
+					data: meantemparray,
+					borderColor: 'black',
+					borderWidth: 2
+				}]
+			},
+			options: {
+				scales: {
+					yAxes: [{
+						ticks: {
+							callback: function(value, index, values){
+								return value + '°';
+							},
+							beginAtZero: false
+						
+						}
+					}]
+				}
+			}
+		});
+}
 
 
